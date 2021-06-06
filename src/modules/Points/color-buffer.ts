c8d7578fe37c40ca124448f585e23c14ccb60b55
Stukova Olya <stukova.o@gmail.com>
2021-06-06 09:29:10 +0500

    Ts fixes

--- src/modules/Points/color-buffer.ts
+++ src/modules/Points/color-buffer.ts
@@ -1,14 +1,14 @@
 import regl from 'regl'
 import { StringAccessor } from '@/graph/config'
 import { getValue, getRgbaColor } from '@/graph/helper'
-import { Node } from '@/graph/types'
+import { InputNode, Node } from '@/graph/types'
 import { defaultNodeColor } from '@/graph/variables'
 
-export function createColorBuffer <N extends Node> (
-  nodes: N[],
+export function createColorBuffer <N extends InputNode> (
+  nodes: Node<N>[],
   reglInstance: regl.Regl,
   textureSize: number,
-  colorAccessor: StringAccessor<N>
+  colorAccessor: StringAccessor<Node<N>>
 ): regl.Framebuffer2D {
   const initialState = new Float32Array(textureSize * textureSize * 4)
 
