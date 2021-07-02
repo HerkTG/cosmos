632c0c83dd241d315154cda4f53dead69a16c7ab
Stukova Olya <stukova.o@gmail.com>
2021-07-02 20:30:49 +0500

    Add unknow options to InputNode and InputLink

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
 export type InputNode = {
+  [key: string]: unknown;
   id: string;
   x?: number;
   y?: number;
 }
 
 export type InputLink = {
+  [key: string]: unknown;
   source: string;
   target: string;
 }
